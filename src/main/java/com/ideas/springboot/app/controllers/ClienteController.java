f98bbbf1149a16222d520e8e7cc69c8c8629a016
israel <israel.bejarano.a@gmail.com>
2019-08-01 16:18:18 +0200

    add javadoc

--- src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
@@ -9,18 +9,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ideas.springboot.app.models.dao.IClienteDao;
 
+/**
+ * The Class ClienteController.
+ * @author Israel Bejarano
+ */
 @Controller
 public class ClienteController {
 	
+	/** The cliente dao. */
 	@Autowired
 	@Qualifier("clienteDaoJPA")
 	private IClienteDao clienteDao;
 	
+	/**
+	 * Listar.
+	 *
+	 * @param model the model
+	 * @return the string
+	 */
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "listado de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
-
 }
