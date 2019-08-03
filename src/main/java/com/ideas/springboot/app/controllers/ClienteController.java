0a11ea8ee3f0703e30095b2faa3ab8bffd51542b
israel <israel.bejarano.a@gmail.com>
2019-08-03 15:53:06 +0200

    Servicio UploadFileService para separar el manejo de archivos

--- src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideas/springboot/app/controllers/ClienteController.java
@@ -1,21 +1,13 @@
 package com.ideas.springboot.app.controllers;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -36,6 +28,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ideas.springboot.app.models.entity.Cliente;
 import com.ideas.springboot.app.models.service.IClienteService;
+import com.ideas.springboot.app.models.service.IUploadFileService;
 import com.ideas.springboot.app.util.paginator.PageRender;
 
 /**
@@ -51,11 +44,9 @@ public class ClienteController {
 	@Autowired
 	private IClienteService clienteService;
 	
-	/** The log. */
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	
-	/** The Constant UPLOADS_FOLDER. */
-	private final static String UPLOADS_FOLDER = "uploads";
+	/** The upload file service. */
+	@Autowired
+	private IUploadFileService uploadFileService;
 	
 	/**
 	 * Ver foto.
@@ -65,14 +56,9 @@ public class ClienteController {
 	 */
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
-		Path pathFoto = Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
-		log.info("pathFoto: " + pathFoto);
 		Resource recurso = null;
 		try {
-			recurso = new UrlResource(pathFoto.toUri());
-			if(!recurso.exists() || !recurso.isReadable()) {
-				throw new RuntimeException("Error: no se puede cargar la imagen: " + pathFoto.toString());
-			}
+			recurso = uploadFileService.load(filename);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
@@ -186,25 +172,16 @@ public class ClienteController {
 			
 			//borrar foto por si el usuario ya tuviese foto asi eliminamos archivos basura
 			if(cliente.getId() != null && cliente.getId() > 0 && cliente.getFoto() != null && cliente.getFoto().length() > 0) {
-				Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
-				File archivo = rootPath.toFile();
-				if(archivo.exists() && archivo.canRead()) {
-					archivo.delete();
-				}
+				uploadFileService.delete(cliente.getFoto());
 			}
-			
-			String uniqueFileName =  UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
-			Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(uniqueFileName);
-			Path rootAbsolutePath = rootPath.toAbsolutePath();
-			log.info("rootPath: " + rootPath);	// path relativo al proyecto
-			log.info("rootAbsolutePath: " + rootAbsolutePath);	// path absoluto
+			String uniqueFileName = null;
 			try {
-				Files.copy(foto.getInputStream(), rootAbsolutePath);
-				flash.addFlashAttribute("info", "Has subido correctamente '" + foto.getOriginalFilename() + "'");
-				cliente.setFoto(uniqueFileName);
+				uniqueFileName = uploadFileService.copy(foto);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			flash.addFlashAttribute("info", "Has subido correctamente '" + foto.getOriginalFilename() + "'");
+			cliente.setFoto(uniqueFileName);
 		}
 		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito" : "Cliente creado con éxito";
 		clienteService.save(cliente);
@@ -224,17 +201,11 @@ public class ClienteController {
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if(id > 0) {
 			Cliente cliente = clienteService.findOne(id);
-			
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
-			
 			//borrar la foto asociada al cliente
-			Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
-			File archivo = rootPath.toFile();
-			if(archivo.exists() && archivo.canRead()) {
-				if(archivo.delete()) {
-					flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con éxito");
-				}
+			if(uploadFileService.delete(cliente.getFoto())) {
+				flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con éxito");
 			}
 		}
 		return "redirect:/listar";
---
+++ src/main/java/com/ideas/springboot/app/models/service/IUploadFileService.java
@@ -0,0 +1,38 @@
+package com.ideas.springboot.app.models.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * The Interface IUploadFileService. Se encarga del manejo de ficheros.
+ * @author Israel Bejarano
+ */
+public interface IUploadFileService {
+	
+	/**
+	 * Load.
+	 *
+	 * @param filename the filename
+	 * @return the resource
+	 */
+	public Resource load(String filename) throws MalformedURLException;
+	
+	/**
+	 * Copy.
+	 *
+	 * @param file the file
+	 * @return the string
+	 */
+	public String copy(MultipartFile file) throws IOException;
+	
+	/**
+	 * Delete.
+	 *
+	 * @param filename the filename
+	 * @return true, if successful
+	 */
+	public boolean delete(String filename);
+}
