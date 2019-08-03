e1164d7200e97b35b3cbb09b5d1428989ed8f3d2
israel <israel.bejarano.a@gmail.com>
2019-08-03 16:45:27 +0200

    Entidad Factura

---
+++ src/main/java/com/ideas/springboot/app/models/entity/Factura.java
@@ -0,0 +1,146 @@
+package com.ideas.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * The Class Factura.
+ * @author Israel Bejarano
+ */
+@Entity
+@Table(name = "facturas")
+public class Factura implements Serializable {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The id. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	/** The descripcion. */
+	private String descripcion;
+	
+	/** The observacion. */
+	private String observacion;
+	
+	/** The create at. */
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	/** The cliente. */
+	@ManyToOne(fetch = FetchType.LAZY) // carga perezosa, evita que traiga todo de una sola vez, es lo recomendado
+	private Cliente cliente;
+	
+	/**
+	 * Pre persist.
+	 */
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the descripcion.
+	 *
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	/**
+	 * Sets the descripcion.
+	 *
+	 * @param descripcion the new descripcion
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 * Gets the observacion.
+	 *
+	 * @return the observacion
+	 */
+	public String getObservacion() {
+		return observacion;
+	}
+
+	/**
+	 * Sets the observacion.
+	 *
+	 * @param observacion the new observacion
+	 */
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	/**
+	 * Gets the creates the at.
+	 *
+	 * @return the creates the at
+	 */
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	/**
+	 * Sets the creates the at.
+	 *
+	 * @param createAt the new creates the at
+	 */
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	/**
+	 * Gets the cliente.
+	 *
+	 * @return the cliente
+	 */
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	/**
+	 * Sets the cliente.
+	 *
+	 * @param cliente the new cliente
+	 */
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+}
