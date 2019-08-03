33b0e9b734374174d64a4326408ea492ac6c023e
israel <israel.bejarano.a@gmail.com>
2019-08-03 17:11:47 +0200

    relación con ItemFactura

--- src/main/java/com/ideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/ideas/springboot/app/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.ideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -46,6 +51,18 @@ public class Factura implements Serializable {
 	@ManyToOne(fetch = FetchType.LAZY) // carga perezosa, evita que traiga todo de una sola vez, es lo recomendado
 	private Cliente cliente;
 	
+	/** The items. */
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id") // para generar la llave foreanea factura_id en la tabla facturas_items
+	private List<ItemFactura> items;
+	
+	/**
+	 * Instantiates a new factura.
+	 */
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
+
 	/**
 	 * Pre persist.
 	 */
@@ -142,5 +159,32 @@ public class Factura implements Serializable {
 	 */
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	/**
+	 * Gets the items.
+	 *
+	 * @return the items
+	 */
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	/**
+	 * Sets the items.
+	 *
+	 * @param items the new items
+	 */
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+	
+	/**
+	 * Adds the item factura.
+	 *
+	 * @param item the item
+	 */
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
 	}
 }
