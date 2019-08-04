2fd512af5a1f914bd4db5239768c101afc7a4dae
israel <israel.bejarano.a@gmail.com>
2019-08-04 18:38:35 +0200

    guardar factura en BBDD usando jquery

--- src/main/java/com/ideas/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/ideas/springboot/app/controllers/FacturaController.java
@@ -3,17 +3,23 @@ package com.ideas.springboot.app.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ideas.springboot.app.models.entity.Cliente;
 import com.ideas.springboot.app.models.entity.Factura;
+import com.ideas.springboot.app.models.entity.ItemFactura;
 import com.ideas.springboot.app.models.entity.Producto;
 import com.ideas.springboot.app.models.service.IClienteService;
 
@@ -29,6 +35,9 @@ public class FacturaController {
 	/** The cliente service. */
 	@Autowired
 	private IClienteService clienteService;
+	
+	/** The log. */
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * Crear.
@@ -61,5 +70,35 @@ public class FacturaController {
 	@GetMapping(value = "/cargar-productos/{term}", produces= {"application/json"})
 	public @ResponseBody List<Producto> cargarProductos(@PathVariable String term) {
 		return clienteService.findByNombre(term);
+	}
+	
+	/**
+	 * Guardar.
+	 *
+	 * @param factura the factura
+	 * @param itemId the item id
+	 * @param cantidad the cantidad
+	 * @param flash the flash
+	 * @param status the status
+	 * @return the string
+	 */
+	@PostMapping("/form")
+	public String guardar(Factura factura, @RequestParam(name = "item_id[]", required = false) Long[] itemId,
+			@RequestParam(name = "cantidad[]", required = false) Integer[] cantidad, RedirectAttributes flash,
+			SessionStatus status) {
+		for (int i = 0; i < itemId.length; i++) {
+			Producto producto = clienteService.findProductoById(itemId[i]);
+			ItemFactura linea = new ItemFactura();
+			linea.setCantidad(cantidad[i]);
+			linea.setProducto(producto);
+			factura.addItemFactura(linea);
+			log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+		}
+		
+		clienteService.saveFactura(factura);
+		status.setComplete();
+		
+		flash.addFlashAttribute("success", "¡Factura creada con éxito!");
+		return "redirect:/ver/" + factura.getCliente().getId();
 	}
 }
