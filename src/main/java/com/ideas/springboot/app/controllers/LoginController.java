8d75b79511a8b6a7803de8b53f3d6b04b5404585
israel <israel.bejarano.a@gmail.com>
2019-08-05 17:41:52 +0200

    control errores y logout

--- src/main/java/com/ideas/springboot/app/controllers/LoginController.java
+++ src/main/java/com/ideas/springboot/app/controllers/LoginController.java
@@ -19,19 +19,24 @@ public class LoginController {
 	 * Login.
 	 *
 	 * @param error the error
+	 * @param logout the logout
 	 * @param model the model
 	 * @param principal the principal
 	 * @param flash the flash
 	 * @return the string
 	 */
 	@GetMapping("/login")
-	public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value = "error", required = false) String error, 
+			@RequestParam(value = "logout", required = false) String logout, Model model, Principal principal, RedirectAttributes flash) {
 		if(principal != null) {
 			flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
 			return "redirect:/";
 		}
 		if(error != null) {
 			model.addAttribute("error", "Error en el login: nombre de usuario o password incorrecto, por favor vuelva a intentarlo.");
+		}
+		if(logout != null) {
+			model.addAttribute("success", "Sessión cerrada con éxito.");
 		}
 		return "login";
 	}
