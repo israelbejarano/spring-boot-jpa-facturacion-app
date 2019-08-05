41926e076d45b6463e236741877026de59d52ff2
israel <israel.bejarano.a@gmail.com>
2019-08-05 15:36:21 +0200

    solved error de inner join por left join
    muestra el cliente cuando no tenga facturas

--- src/main/java/com/ideas/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/ideas/springboot/app/models/dao/IClienteDao.java
@@ -18,7 +18,7 @@ public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long>{
 	 * @param id the id
 	 * @return the cliente
 	 */
-	@Query("select c from Cliente c join fetch c.facturas f where c.id=?1")
+	@Query("select c from Cliente c left join fetch c.facturas f where c.id=?1")
 	public Cliente fetchByIdWithFacturas(Long id);
 	
 }
