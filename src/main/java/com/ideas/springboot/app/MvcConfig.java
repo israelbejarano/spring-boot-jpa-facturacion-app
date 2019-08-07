2777184a3bd5d224850c39b01c73959216eed194
israel <israel.bejarano.a@gmail.com>
2019-08-07 16:58:24 +0200

    add interceptores de idiomas

--- src/main/java/com/ideas/springboot/app/MvcConfig.java
+++ src/main/java/com/ideas/springboot/app/MvcConfig.java
@@ -88,6 +88,4 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
-	
-	
 }
