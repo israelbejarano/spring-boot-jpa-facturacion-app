9a03f26bd1f780359fca006784c5336cab560a30
israel <israel.bejarano.a@gmail.com>
2019-08-07 16:13:07 +0200

    add insert nuevos para usuarios y roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -42,3 +42,11 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
 
 INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
+
+/* Creamos algunos usuarios con sus roles */
+INSERT INTO `users` (username, password, enabled) VALUES ('israel','$2a$10$Nmsw4WI2XUGagIuzR2fNTOe4QcqVWxOictJfh528tExLAtr2ep/nq',1);
+INSERT INTO `users` (username, password, enabled) VALUES ('admin','$2a$10$R00T60jF6yqdRDnIULMbzO3W2ZtLohpKehK9Fzkv/aMy6dfFRr5UO',1);
+
+INSERT INTO `authorities` (user_id, authority) VALUES (1,'ROLE_USER');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_ADMIN');
+INSERT INTO `authorities` (user_id, authority) VALUES (2,'ROLE_USER');
