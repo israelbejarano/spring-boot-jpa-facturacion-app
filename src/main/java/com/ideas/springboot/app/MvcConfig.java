86e8c0b1596900423f25c4d36aa329a35cbb425a
israel <israel.bejarano.a@gmail.com>
2019-08-08 12:15:07 +0200

    bean para poder convertir el objeto entity a xml

--- src/main/java/com/ideas/springboot/app/MvcConfig.java
+++ src/main/java/com/ideas/springboot/app/MvcConfig.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 //import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -87,5 +88,17 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	/**
+	 * Jaxb 2 marshaller.
+	 *
+	 * @return the jaxb 2 marshaller
+	 */
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[] {});
+		return marshaller;
 	}
 }
